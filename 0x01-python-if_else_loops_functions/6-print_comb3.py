5c62abdbbf4faaa3fe4eb5d6fb201d89b2f3e10e
MercyKorir <mcthegreat78@gmail.com>
2022-08-23 09:15:03 -0700

    I have added the file 6*

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i != j:
+            if i == 8 and j == 9:
+                print("{}{}".format(i, j))
+            else:
+                print("{}{}".format(i, j), end=", ")
