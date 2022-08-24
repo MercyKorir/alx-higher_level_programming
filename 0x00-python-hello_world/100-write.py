6945d5107738ed325f53a6430d115bca2320812f
MercyKorir <mcthegreat78@gmail.com>
2022-08-24 03:03:25 -0700

    I have added 100*

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write('and that piece of art is useful - Dora Korpar, 2015-10-19')
-sys.stdout.write('\n')
+sys.stdout.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
 sys.stderr.write("Error occured\n")
