857e77cc8e89c07ee0b5e59bcc0da836e5511b07
MercyKorir <mcthegreat78@gmail.com>
2022-08-25 02:26:48 -0700

    I have added the file 1*

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    c = add(10, 5)
-    d = sub(10, 5)
-    e = mul(10, 5)
-    f = div(10, 5)
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, c))
-    print("{} - {} = {}".formar(a, b, d))
-    print("{} * {} = {}".format(a, b, e))
-    print("{} / {} = {}".format(a, b, f))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".formar(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
