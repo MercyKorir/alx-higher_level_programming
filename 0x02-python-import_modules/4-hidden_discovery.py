16af6d82fb4a30cd10fbaf0332905b3a74207d53
MercyKorir <mcthegreat78@gmail.com>
2022-08-25 03:07:59 -0700

    I have added 4*

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,5 @@ import hidden_4
 if __name__ == "__main__":
     name = dir(hidden_4)
     for i in name:
-        if i[0:2] == "__":
-            continue
-        print(i):
+        if i[0:2] != "__":
+            print(i):
