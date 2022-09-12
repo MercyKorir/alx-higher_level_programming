d2f56d00f3934214247ec8157bc4d4fc28caf852
MercyKorir <mcthegreat78@gmail.com>
2022-09-12 07:10:13 -0700

    I have added the file 0*

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             print("{}".format(i), end="")
-            x += 1
+            count += 1
     except IndexError:
         print("An Error occured")
     return count
