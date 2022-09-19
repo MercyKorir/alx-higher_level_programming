2f529afb6a9a2b0072b3382146ad6a1f959798a0
MercyKorir <mcthegreat78@gmail.com>
2022-09-19 07:13:22 -0700

    I have made some changes on 1*

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,27 +3,26 @@
 
 
 class Rectangle:
-    """Contents of a Reactangle represented here"""
+    """Contains the attributes of a rectangle"""
+
     def __init__(self, width=0, height=0):
-        self.__height = height
         self.__width = width
-
+        self.__height = height
+    def width(self):
+        return self.width
+    def width(self, value):
+        self.width = value
+        result = isinstance(value, int)
+        if result == False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 9")
     def height(self):
-        return self.__height
-
+        return self.height
     def height(self, value):
-        self.__height = value
+        self.height = value
         res = isinstance(value, int)
         if res == False:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-    def width(self):
-        return self.__width
-    def width(self, value):
-        self.__width = value
-        result = isinstance(value, int)
-        if result == False:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
