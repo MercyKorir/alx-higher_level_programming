d04dfca3f5f3d22a4c8ebd4c9531969d8ba8174b
MercyKorir <mcthegreat78@gmail.com>
2022-09-19 06:53:58 -0700

    I have made documentation style error corrections on file 1*

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,19 +5,8 @@
 class Rectangle:
     """Contents of a Reactangle represented here"""
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
-
-    def width(self):
-        return self.__width
-
-    def width(self, value):
-        self.__width = value
-        result = isinstance(value, int)
-        if result == False:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        self.__width = width
 
     def height(self):
         return self.__height
@@ -29,3 +18,12 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+    def width(self):
+        return self.__width
+    def width(self, value):
+        self.__width = value
+        result = isinstance(value, int)
+        if result == False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
