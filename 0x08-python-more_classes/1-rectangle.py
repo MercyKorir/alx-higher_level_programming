8d0af621a59fd2b379e04dcd226b98eaadcbc624
MercyKorir <mcthegreat78@gmail.com>
2022-09-19 07:19:54 -0700

    I have made some changes on 1*

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,11 @@ class Rectangle:
     """Contains the attributes of a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initialize new object"""
+        """Initialize new object
+        Args:
+            width (int): Width of object
+            height (int): Height of new object
+        """
         self.__height = height
         self.__width = width
     def width(self):
