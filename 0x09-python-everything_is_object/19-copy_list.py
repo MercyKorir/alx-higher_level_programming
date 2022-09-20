35ba065a73fce8a881a00912b9a40bd82506e77a
MercyKorir <mcthegreat78@gmail.com>
2022-09-20 02:11:55 -0700

    I have made corrections to file 19*

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-"""Definition of a function that copies a list"""
-
-
 def copy_list(l):
-    """Takes the list copies it and returns a copy"""
-
-    return (l[:])
+    return l[:]
