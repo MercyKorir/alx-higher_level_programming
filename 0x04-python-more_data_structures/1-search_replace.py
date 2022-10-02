b5c4df5eea5827ac99bf5db733f1b21c15f0a413
MercyKorir <mcthegreat78@gmail.com>
2022-10-02 09:52:30 -0700

    I have added the file 1*

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     length = len(my_list)
     new_list = my_list[:]
-    for element in len(new_list):
+    for element in range(length):
         if element == search:
             element = replace
         else:
