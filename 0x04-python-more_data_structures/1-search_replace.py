82c2207527cb41aafb874975218d7d9ba838848d
MercyKorir <mcthegreat78@gmail.com>
2022-10-02 09:56:05 -0700

    I have added the file 1*

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    length = len(my_list)
     new_list = my_list[:]
+    length = len(new_list)
     for element in range(length):
         if new_list[element] == search:
             new_list[element] = replace
-        else:
-            new_list[element] = element
     return (new_list)
