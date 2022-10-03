5546d3d089679f648ef8b5df37829aa304b97aa1
MercyKorir <mcthegreat78@gmail.com>
2022-10-03 01:09:56 -0700

    I have added the file 12*

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,10 +8,17 @@ def roman_to_int(roman_string):
         return (0)
     if roman_string is None:
         return (0)
-    roman_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000}
     while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i: i+2] in roman_values:
-            int_val += roman_values[roman_string[i: i+2]]
+        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_values:
+            int_val += roman_values[roman_string[i:i+2]]
             i += 2
         else:
             int_val += roman_values[roman_string[i]]
