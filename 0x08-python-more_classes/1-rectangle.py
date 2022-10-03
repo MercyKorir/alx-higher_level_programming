ac55ed35a5e81f1ec386ff0008dd605c827a1aa8
MercyKorir <mcthegreat78@gmail.com>
2022-10-03 13:22:14 -0700

    I have added the file 1*

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -29,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
