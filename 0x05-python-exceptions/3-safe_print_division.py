1f7aac44c1a0409a3e070c1119211e44724a740c
MercyKorir <mcthegreat78@gmail.com>
2022-10-03 02:01:38 -0700

    I have added the file 3*

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, TypeError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div), end="\n")
+    return (div)
