0cb5a7749302b2ec687c7f4928907a0291e78e0f
MercyKorir <mcthegreat78@gmail.com>
2022-10-03 02:43:00 -0700

    I have added the file 1*

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Definition of the class Square"""
+
+
+class Square:
+    """The declaration of the private instance attribute"""
+
+    def __init__(self, size):
+        self.__size = size
