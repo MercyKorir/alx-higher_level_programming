fde5114d37aa56abb1c84b754bd838c5163a5449
MercyKorir <mcthegreat78@gmail.com>
2022-10-03 00:02:05 -0700

    I have added the file 9*

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[k] *= 2
-    return (a_dictionary)
+    dict_cpy = {:}
+    for i in dict_cpy:
+        dict_cpy[i] *= 2
+    return (dict_cpy)
