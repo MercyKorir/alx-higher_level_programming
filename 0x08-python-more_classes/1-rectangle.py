f67f468a3b3bffce4cb521af50dbe5a0ab77df4a
MercyKorir <mcthegreat78@gmail.com>
2022-10-03 13:17:09 -0700

    I have added the file 1*

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,25 +6,35 @@ class Rectangle:
     """Contains the attributes of a rectangle"""
 
     def __init__(self, width=0, height=0):
+        """Declaration of attributes height and width
+
+        Args:
+            height (int): height of the rectangle
+            width (int): width of the rectangle
+        """
         self.__height = height
         self.__width = width
 
+    @property
     def width(self):
         return self.__width
 
+    @width.setter
     def width(self, value):
         self.__width = value
-        if isinstance(value, int) is False:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 9")
+            raise ValueError("width must be >= 0")
 
+    @property
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
         self.__height = value
-        if isinstance(value, int) is False:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
