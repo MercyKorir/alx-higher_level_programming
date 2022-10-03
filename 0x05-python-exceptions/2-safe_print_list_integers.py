805bd2153ac2109b8544b4e6827f206ec5c708b5
MercyKorir <mcthegreat78@gmail.com>
2022-10-03 01:49:11 -0700

    I have adde the file 2*

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
-            break
+            continue
     print("")
     return (count)
