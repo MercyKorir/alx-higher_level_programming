9c452d98e0b07cfbe3a9a6cca33df58e3a63cd67
MercyKorir <mcthegreat78@gmail.com>
2022-11-02 21:24:59 +0300

    I have added the file for task 9*

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS second_table(
 	score INT
 )
 INSERT INTO second_table(id, name, score)
-VALUES(1, 'John', 10),
+VALUES
+	(1, 'John', 10),
 	(2, 'Alex', 3),
 	(3, 'Bob', 14),
 	(4, 'George', 8);
