75c224cecb66e1d04196fa131662a82be1bf1445
MercyKorir <mcthegreat78@gmail.com>
2022-11-02 23:46:13 +0300

    I have added 6*

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- This script creates a database "hbtn_0d_usa" and table "states"
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-	id INT NOT UNIQUE AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT NOT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
 );
