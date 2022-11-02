c4cac61fd6be524888891464cc30cefb7775b9fe
MercyKorir <mcthegreat78@gmail.com>
2022-11-02 22:32:13 +0300

    I have added the file for task 17*

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,3 @@
 -- This script converts hbtn_0c_0 to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table
-MODIFY name utf8 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
