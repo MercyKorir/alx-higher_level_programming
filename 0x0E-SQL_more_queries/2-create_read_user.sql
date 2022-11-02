a5fab10502e136a79835c9dfd2681a7228a1c78b
MercyKorir <mcthegreat78@gmail.com>
2022-11-02 23:26:36 +0300

    I have added the file 2*

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- This script creates a databse hbtn_0d_2 and user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
