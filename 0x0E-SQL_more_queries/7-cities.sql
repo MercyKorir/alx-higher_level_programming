8fc94762640ee5fed4fc242ca505f26296153986
MercyKorir <mcthegreat78@gmail.com>
2022-11-02 23:58:03 +0300

    I have added the file 7*

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- This script creates database "hbtn_0d_usa" and table "cities"
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id)
+);
