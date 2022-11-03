03ad9b6e11b08f106752c3fed082b285e3a2990b
MercyKorir <mcthegreat78@gmail.com>
2022-11-03 08:49:08 +0300

    I have added the file 13*

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- This script lists all genres from hbtn_0d_tvshows
-SELECT tv_genres.name COUNT(*)
+SELECT tv_genres.name AS genre COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.`id` = tv_show_genres.`genre_id`
