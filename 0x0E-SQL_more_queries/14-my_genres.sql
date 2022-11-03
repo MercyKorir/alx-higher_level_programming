1ed93f57c5d33ee6f931b496ef73ebbc7296f5a2
MercyKorir <mcthegreat78@gmail.com>
2022-11-03 08:59:06 +0300

    I have added 14*

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,5 +5,5 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-WHERE t.title = 'Dexter'
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
