940454ea3601ff8345c397a5f554533db865a1df
MercyKorir <mcthegreat78@gmail.com>
2022-11-03 09:08:20 +0300

    I have added the file 16*

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- This script lists all shows and genres linked to that show
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
