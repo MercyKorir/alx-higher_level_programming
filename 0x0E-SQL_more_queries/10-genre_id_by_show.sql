b30f14197257ac4fac503083bf2f20d508475fc6
MercyKorir <mcthegreat78@gmail.com>
2022-11-03 08:31:24 +0300

    I have added the file 10*

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- This script imports database dump from hbtn_0d_tvshows
+SELECT tvshows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
