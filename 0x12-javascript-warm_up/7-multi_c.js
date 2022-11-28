37bda5847e015d916d6aa95baeb5b5893af991d2
MercyKorir <mcthegreat78@gmail.com>
2022-11-28 11:05:40 +0300

    I have added the file for 7*

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const len = argv.length;
-let intArg;
+let x;
 if (len === 2) {
-  console.log('Not a number');
+  console.log('Missing number of occurrences');
 }
 argv.forEach((val, index) => {
   if (index === 2) {
     if (isNaN(val)) {
       console.log('Not a number');
     } else {
-      intArg = Math.floor(Number(val));
-      console.log(`My number: ${intArg}`);
+      x = Math.floor(Number(val));
+      for (let iter = 0; iter < x; iter++) {
+        console.log('C is fun');
+      }
     }
   }
 });
