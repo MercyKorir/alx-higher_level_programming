eec5ee95285379bf3b340b1922fe2c62d886d7bd
MercyKorir <mcthegreat78@gmail.com>
2022-11-28 09:34:17 +0300

    I have added the file for 1*

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const firstWord = 'C is fun';
+const secondWord = 'Python is cool';
+const thirdWord = 'JavaScript is amazing';
+console.log(firstWord + '\n' + secondWord + '\n' + thirdWord);
