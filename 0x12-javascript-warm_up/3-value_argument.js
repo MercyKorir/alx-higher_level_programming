354e415d285b79801ecec874fd6d19f036bca81b
MercyKorir <mcthegreat78@gmail.com>
2022-11-29 10:08:41 +0300

    Made corrections in 3*

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 let count = 0;
-argv.forEach((val, index) => {
+process.argv.forEach((val, index) => {
   count++;
 });
 if (count === 2) {
   console.log('No argument');
 } else {
-  argv.forEach((val, index) => {
+  process.argv.forEach((val, index) => {
     if (index === 2) {
       console.log(`${val}`);
     }
