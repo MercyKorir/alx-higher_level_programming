15c3042b9108844f9b2182182acf7e0c917b39e5
MercyKorir <mcthegreat78@gmail.com>
2022-11-29 10:36:13 +0300

    I have added 9*

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
-
+#!/usr/bin/node
+function add (a, b) {
+  const len = process.argv.length;
+  if (len <= 3) {
+    return ('NaN');
+  } else {
+    return (Number(a) + Number(b));
+  }
+}
+console.log(add(process.argv[2], process.argv[3]));
