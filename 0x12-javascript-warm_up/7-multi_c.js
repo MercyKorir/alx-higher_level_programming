2fb3b19e5a1c9ccce9ab7777fa2186c56e77c188
MercyKorir <mcthegreat78@gmail.com>
2022-11-29 10:18:08 +0300

    Made some correnctions in 7*

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const len = argv.length;
+const len = process.argv.length;
 let x;
 if (len === 2) {
   console.log('Missing number of occurrences');
 }
-argv.forEach((val, index) => {
+process.argv.forEach((val, index) => {
   if (index === 2) {
-    if (isNaN(val)) {
-      console.log('Not a number');
-    } else {
-      x = Math.floor(Number(val));
-      for (let iter = 0; iter < x; iter++) {
-        console.log('C is fun');
-      }
+    x = Math.floor(Number(val));
+    for (let iter = 0; iter < x; iter++) {
+      console.log('C is fun');
     }
   }
 });
