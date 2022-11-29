a778d610eebbabb9e982d5f9771c539dd67aba38
MercyKorir <mcthegreat78@gmail.com>
2022-11-29 10:12:26 +0300

    Made corrections in 6*

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const words = [];
-words.push('C is fun', 'Python is cool', 'Javascript is amazing');
+words.push('C is fun', 'Python is cool', 'JavaScript is amazing');
 words.forEach((val) => {
   console.log(`${val}`);
 });
