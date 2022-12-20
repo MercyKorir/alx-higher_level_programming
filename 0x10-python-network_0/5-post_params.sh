fc686ff5258f11d84b5a007b09082e2c5db43bd8
MercyKorir <mcthegreat78@gmail.com>
2022-12-20 14:36:47 +0300

    I have added the file 5*

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes url, sends post request and displays response body
-curl -s -X POST "$1" -d "email=test@gmail.com" -d"subject=I will always be here for PLD"
+curl -s -X POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
