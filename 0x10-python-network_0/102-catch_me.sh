c684d790b8039fd6001d242a4f33aa47d9d0519c
MercyKorir <mcthegreat78@gmail.com>
2022-12-20 04:26:21 -0800

    I have added the file for 102*

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #makes request to specific url and responds with message
-curl -s -L -X PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" -d "user_id=98"
+curl -s -L -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
