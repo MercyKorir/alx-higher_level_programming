4ad42f90fb8661360d51f4fb0399a035564f6f3b
MercyKorir <mcthegreat78@gmail.com>
2022-12-20 04:13:17 -0800

    I have added file for 101*

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends JSON POST request to url and displays response body
-curl -s "$1" -H "Content-Type: application/json" -H "Accept: application/json" -d "[json data]"
+curl -s "$1" -H "Content-Type: application/json" -d "$(cat "$2")"
