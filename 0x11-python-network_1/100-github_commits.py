cb325162e5d44aeb9fff12e92f057b4f142647f5
MercyKorir <mcthegreat78@gmail.com>
2022-12-24 16:17:21 +0300

    I have added the file for 100*

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script takes 2 args in order to
+solve backend position challenge
+1st arg is repository name
+2nd arg is owner name
+use requests and sys packages
+"""
+
+
+import sys
+import requests
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+            sys.argv[2], sys.argv[1])
+    r = requests.get(url)
+    commits = r.json()
+    for i in range(10):
+        print("{}: {}".format(commits[i].get("sha"),
+            commits[i].get("commit").get("author").get("name")))
