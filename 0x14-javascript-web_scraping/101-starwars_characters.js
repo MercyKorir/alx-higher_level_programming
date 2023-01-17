5a1be48e7e29e67501fd765985c04a78bc92b65d
MercyKorir <mcthegreat78@gmail.com>
2023-01-17 15:59:57 +0300

    I have added the file for 101*

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const id = process.argv[2];
+const request = require('request');
+const url = `https://swapi.dev/api/films/${id}/`;
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    characters.forEach(character => {
+      request(character, (err, res, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
