2c93220127d07d5b1d32b51b929b8a6edddd60d6
MercyKorir <mcthegreat78@gmail.com>
2023-01-17 13:58:58 +0300

    I have added the file for 0*

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const file = process.argv[2];
 const fs = require('fs');
-const path = require('path');
-fs.readFile(path.resolve(file), 'utf8', (err, data) => {
+fs.readFile(file, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
