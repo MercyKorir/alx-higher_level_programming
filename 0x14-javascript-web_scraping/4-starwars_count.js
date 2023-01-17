0e2d78d18e0dae60149dec6a04452de83fc57808
MercyKorir <mcthegreat78@gmail.com>
2023-01-17 14:34:46 +0300

    I have added the file for 4*

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+const id = 18;
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    let count = 0;
+    const result = JSON.parse(body);
+    result.results.forEach((movie) => {
+      if (movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+        count++;
+      }
+    });
+    console.log(`${count}`);
+  }
+});
