368fb3d2d0e4acfa08952f580a0d7ea3cfbe1373
MercyKorir <mcthegreat78@gmail.com>
2023-01-18 17:41:04 +0300

    I have added the file for 14*

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""contains class definition of a City and an"""
+"""contains class definition of a City"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
 
 class City(Base):
+    """Declaration of class City
+
+    __tablename__(str): name of table to store values
+    id (sqlalchemy.Integer): id of city
+    name (sqlalchemy.String): name of city
+    """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
